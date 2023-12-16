a756846bd267888e378c485941c4c1381e6c683b
Davide Molitierno <>
2023-12-16 11:21:26 +0100

    Commit iniziale

---
+++ InfinityDropper/Assets/Scripts/PlayerDestroyer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroyer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
